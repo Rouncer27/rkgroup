5a8db71d4bf0ef63929212d17ca0eef44ba8f2e2
Rouncer27 <trevor@switchbackcreative.ca>
2020-06-16 14:18:28 -0600

    setup for wordpress cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,11 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `White Fields Calvary Church`,
+    description: `White Fields Calvary Church`,
+    author: `@switchback4ever`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,6 +30,23 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        baseUrl: process.env.GATSBY_URL,
+        protocol: process.env.GATSBY_PROTOCOL,
+        hostingWPCOM: false,
+        useACF: true,
+        includedRoutes: [
+          "**/posts",
+          "**/media",
+          "**/pages",
+          "**/wp-api-menus/v2",
+          "**/wp-api-menus/v2/menus",
+        ],
+      },
+    },
+    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
